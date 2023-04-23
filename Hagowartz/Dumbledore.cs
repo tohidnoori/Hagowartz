4aac837973ed8b7aca22a784c6ffe1518bf5f03b
ASUS <ASUS@DESKTOP-2AL4IC0>
2023-04-23 21:03:37 +0330

    Add project files.

---
+++ Hagowartz/Dumbledore.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hagowartz
+{
+    internal class Dumbledore:AuthorizedPersons
+    {
+        public List<Lesson> LessonList { get; set; }
+
+        public int CurrentTerm { get; set; }
+        public int DormNum { get; set; }
+    }
+}
---
+++ Hagowartz/Group.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hagowartz
+{
+    public enum EGroupType
+    { Hufflepuff, Gryffindor, Ravenclaw, Slytherin}
+    internal class Group
+    {
+        public int Score { get; set; }
+        public EGroupType Type { get; set; }
+        public List<AuthorizedPersons> GroupMembers { get; set; }
+        public List<AuthorizedPersons> Qoidich { get; set; }
+    }
+}
